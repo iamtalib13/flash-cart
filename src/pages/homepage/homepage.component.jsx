9fcb9355af0b0aeceeeac02b98e4342f9c2721f2
Talib sheikh <talibsheikh16@gmail.com>
2021-09-06 21:24:02 +0530

    broken our homepage into smaller components, finished styling our menu-item component

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import Direcotry from "../../components/directory/directory.component";
+import "./homepage.styles.scss";
+
+export const HomePage = () => (
+  <div className="homepage">
+    <Direcotry />
+  </div>
+);
